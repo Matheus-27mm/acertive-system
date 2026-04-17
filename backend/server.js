ab7476ed92fb2031b67d57f9c0467f15becab70c
Matheus <matheusmenezes2003.mm@gmail.com>
2026-04-17 15:09:43 -0300

    atualizacao sistema

--- backend/server.js
+++ backend/server.js
@@ -62,8 +62,8 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 10 * 1024 * 1024 } });
